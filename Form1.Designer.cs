6f25f97eebf6e5fe0e88f2ed90b830d9895c9b0c
berkeyucetas <berkeeyucetass@gmail.com>
2023-10-17 22:57:27 +0300

    Hayvanlar ve Sesleri Bir Günde Bir Proje #20
    Okul öncesi çağı cocuklar için basitt bir hayvanları tanıma seslerine ve ingilizcelerine aşina olmak için bir proje geliştirdim

---
+++ Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace Hayvanlar_ve_Sesleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureat = new System.Windows.Forms.PictureBox();
+            this.pictureesek = new System.Windows.Forms.PictureBox();
+            this.picturehoroz = new System.Windows.Forms.PictureBox();
+            this.pictureinek = new System.Windows.Forms.PictureBox();
+            this.picturekedi = new System.Windows.Forms.PictureBox();
+            this.picturekus = new System.Windows.Forms.PictureBox();
+            this.pictureordek = new System.Windows.Forms.PictureBox();
+            this.picturemaymun = new System.Windows.Forms.PictureBox();
+            this.picturekoyun = new System.Windows.Forms.PictureBox();
+            this.picturekopek = new System.Windows.Forms.PictureBox();
+            this.labelturkce = new System.Windows.Forms.Label();
+            this.labelenglish = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureesek)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturehoroz)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureinek)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturekedi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturekus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureordek)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturemaymun)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturekoyun)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturekopek)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureat
+            // 
+            this.pictureat.Image = ((System.Drawing.Image)(resources.GetObject("pictureat.Image")));
+            this.pictureat.Location = new System.Drawing.Point(12, 76);
+            this.pictureat.Name = "pictureat";
+            this.pictureat.Size = new System.Drawing.Size(120, 120);
+            this.pictureat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureat.TabIndex = 0;
+            this.pictureat.TabStop = false;
+            this.pictureat.Click += new System.EventHandler(this.pictureat_Click);
+            // 
+            // pictureesek
+            // 
+            this.pictureesek.Image = ((System.Drawing.Image)(resources.GetObject("pictureesek.Image")));
+            this.pictureesek.Location = new System.Drawing.Point(138, 76);
+            this.pictureesek.Name = "pictureesek";
+            this.pictureesek.Size = new System.Drawing.Size(120, 120);
+            this.pictureesek.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureesek.TabIndex = 1;
+            this.pictureesek.TabStop = false;
+            this.pictureesek.Click += new System.EventHandler(this.pictureesek_Click);
+            // 
+            // picturehoroz
+            // 
+            this.picturehoroz.Image = ((System.Drawing.Image)(resources.GetObject("picturehoroz.Image")));
+            this.picturehoroz.Location = new System.Drawing.Point(264, 76);
+            this.picturehoroz.Name = "picturehoroz";
+            this.picturehoroz.Size = new System.Drawing.Size(120, 120);
+            this.picturehoroz.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picturehoroz.TabIndex = 2;
+            this.picturehoroz.TabStop = false;
+            this.picturehoroz.Click += new System.EventHandler(this.picturehoroz_Click);
+            // 
+            // pictureinek
+            // 
+            this.pictureinek.Image = ((System.Drawing.Image)(resources.GetObject("pictureinek.Image")));
+            this.pictureinek.Location = new System.Drawing.Point(390, 76);
+            this.pictureinek.Name = "pictureinek";
+            this.pictureinek.Size = new System.Drawing.Size(120, 120);
+            this.pictureinek.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureinek.TabIndex = 3;
+            this.pictureinek.TabStop = false;
+            this.pictureinek.Click += new System.EventHandler(this.pictureinek_Click);
+            // 
+            // picturekedi
+            // 
+            this.picturekedi.Image = ((System.Drawing.Image)(resources.GetObject("picturekedi.Image")));
+            this.picturekedi.Location = new System.Drawing.Point(516, 76);
+            this.picturekedi.Name = "picturekedi";
+            this.picturekedi.Size = new System.Drawing.Size(120, 120);
+            this.picturekedi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picturekedi.TabIndex = 4;
+            this.picturekedi.TabStop = false;
+            this.picturekedi.Click += new System.EventHandler(this.picturekedi_Click);
+            // 
+            // picturekus
+            // 
+            this.picturekus.Image = ((System.Drawing.Image)(resources.GetObject("picturekus.Image")));
+            this.picturekus.Location = new System.Drawing.Point(12, 202);
+            this.picturekus.Name = "picturekus";
+            this.picturekus.Size = new System.Drawing.Size(120, 120);
+            this.picturekus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picturekus.TabIndex = 5;
+            this.picturekus.TabStop = false;
+            this.picturekus.Click += new System.EventHandler(this.picturekus_Click);
+            // 
+            // pictureordek
+            // 
+            this.pictureordek.Image = ((System.Drawing.Image)(resources.GetObject("pictureordek.Image")));
+            this.pictureordek.Location = new System.Drawing.Point(138, 202);
+            this.pictureordek.Name = "pictureordek";
+            this.pictureordek.Size = new System.Drawing.Size(120, 120);
+            this.pictureordek.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureordek.TabIndex = 6;
+            this.pictureordek.TabStop = false;
+            this.pictureordek.Click += new System.EventHandler(this.pictureordek_Click);
+            // 
+            // picturemaymun
+            // 
+            this.picturemaymun.Image = ((System.Drawing.Image)(resources.GetObject("picturemaymun.Image")));
+            this.picturemaymun.Location = new System.Drawing.Point(264, 202);
+            this.picturemaymun.Name = "picturemaymun";
+            this.picturemaymun.Size = new System.Drawing.Size(120, 120);
+            this.picturemaymun.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picturemaymun.TabIndex = 7;
+            this.picturemaymun.TabStop = false;
+            this.picturemaymun.Click += new System.EventHandler(this.picturemaymun_Click);
+            // 
+            // picturekoyun
+            // 
+            this.picturekoyun.Image = ((System.Drawing.Image)(resources.GetObject("picturekoyun.Image")));
+            this.picturekoyun.Location = new System.Drawing.Point(390, 202);
+            this.picturekoyun.Name = "picturekoyun";
+            this.picturekoyun.Size = new System.Drawing.Size(120, 120);
+            this.picturekoyun.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picturekoyun.TabIndex = 8;
+            this.picturekoyun.TabStop = false;
+            this.picturekoyun.Click += new System.EventHandler(this.picturekoyun_Click);
+            // 
+            // picturekopek
+            // 
+            this.picturekopek.Image = ((System.Drawing.Image)(resources.GetObject("picturekopek.Image")));
+            this.picturekopek.Location = new System.Drawing.Point(516, 202);
+            this.picturekopek.Name = "picturekopek";
+            this.picturekopek.Size = new System.Drawing.Size(120, 120);
+            this.picturekopek.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picturekopek.TabIndex = 9;
+            this.picturekopek.TabStop = false;
+            this.picturekopek.Click += new System.EventHandler(this.picturekopek_Click);
+            // 
+            // labelturkce
+            // 
+            this.labelturkce.AutoSize = true;
+            this.labelturkce.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)), true);
+            this.labelturkce.Location = new System.Drawing.Point(779, 96);
+            this.labelturkce.Name = "labelturkce";
+            this.labelturkce.Size = new System.Drawing.Size(102, 25);
+            this.labelturkce.TabIndex = 10;
+            this.labelturkce.Text = "Türkçesi:";
+            // 
+            // labelenglish
+            // 
+            this.labelenglish.AutoSize = true;
+            this.labelenglish.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)), true);
+            this.labelenglish.Location = new System.Drawing.Point(767, 132);
+            this.labelenglish.Name = "labelenglish";
+            this.labelenglish.Size = new System.Drawing.Size(114, 25);
+            this.labelenglish.TabIndex = 11;
+            this.labelenglish.Text = "İngilizcesi:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)), true);
+            this.label1.Location = new System.Drawing.Point(912, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)), true);
+            this.label2.Location = new System.Drawing.Point(912, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 12;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(1079, 41);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(26, 16);
+            this.axWindowsMediaPlayer1.TabIndex = 14;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(318, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(538, 48);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "HAYVANLARI TANIYALIM";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)), true);
+            this.label4.Location = new System.Drawing.Point(23, 336);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 25);
+            this.label4.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(803, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 69);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Bilgi Al";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(12, 364);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1128, 194);
+            this.textBox1.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(1164, 570);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelenglish);
+            this.Controls.Add(this.labelturkce);
+            this.Controls.Add(this.picturekopek);
+            this.Controls.Add(this.picturekoyun);
+            this.Controls.Add(this.picturemaymun);
+            this.Controls.Add(this.pictureordek);
+            this.Controls.Add(this.picturekus);
+            this.Controls.Add(this.picturekedi);
+            this.Controls.Add(this.pictureinek);
+            this.Controls.Add(this.picturehoroz);
+            this.Controls.Add(this.pictureesek);
+            this.Controls.Add(this.pictureat);
+            this.Name = "Form1";
+            this.Text = "Hayvanlar ve Sesleri";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureesek)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturehoroz)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureinek)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturekedi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturekus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureordek)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturemaymun)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturekoyun)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturekopek)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureat;
+        private System.Windows.Forms.PictureBox pictureesek;
+        private System.Windows.Forms.PictureBox picturehoroz;
+        private System.Windows.Forms.PictureBox pictureinek;
+        private System.Windows.Forms.PictureBox picturekedi;
+        private System.Windows.Forms.PictureBox picturekus;
+        private System.Windows.Forms.PictureBox pictureordek;
+        private System.Windows.Forms.PictureBox picturemaymun;
+        private System.Windows.Forms.PictureBox picturekoyun;
+        private System.Windows.Forms.PictureBox picturekopek;
+        private System.Windows.Forms.Label labelturkce;
+        private System.Windows.Forms.Label labelenglish;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
